5298f1155f1762815fc8a0d0b1e05f2ab2d588aa
yuchen <smirkiiiv@gmail.com>
2020-04-02 16:08:41 +0800

    Initial Commit

---
+++ swiftUIFirstMeet/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  swiftUIFirstMeet
+//
+//  Created by 林祐辰 on 2020/4/2.
+//  Copyright © 2020 smirkiiiv. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
